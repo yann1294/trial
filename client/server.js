769c37be31504e1664e3657c4f886d53e41967dc
yann1294 <36128975+yann1294@users.noreply.github.com>
2023-02-25 16:39:22 +0530

    Updated server.js once

--- client/server.js
+++ client/server.js
@@ -6,6 +6,21 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const csrfMiddleware = async (req, res, next) => {
+  try {
+    const response = await fetch('hhttps://squid-app-xbx74.ondigitalocean.app/api/csrf-token', { credentials: 'include' });
+    const { csrfToken } = await response.json();
+    req.headers['x-csrf-token'] = csrfToken;
+
+    // Set the Access-Control-Allow-Origin header to the specific origin that is allowed to access the resource
+    res.setHeader('Access-Control-Allow-Origin', 'https://trial-nine-indol.vercel.app');
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 app
   .prepare()
   .then(() => {
