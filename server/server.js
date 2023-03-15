2e3e70ce7b3cf86d82850b54d67f703faf8177d3
yann1294 <36128975+yann1294@users.noreply.github.com>
2023-03-16 01:39:10 +0530

    Updated server.js endpoints

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 import express from "express";
-import cors from "cors";          // how does this work and what's its use?
-import { readdirSync } from "fs"; // how does this work and what's its use?
+import cors from "cors";
+import { readdirSync } from "fs";
 import mongoose from "mongoose";
-import csrf from "csurf";  // how does this work and what's its use?
-import cookieParser from "cookie-parser"; // how does this work and what's its use?
-
-
-// this is the main module   
-
-
-const morgan = require("morgan"); // what is morgan for?
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
+const morgan = require("morgan");
 require("dotenv").config();
 
-const csrfProtection = csrf({ cookie: true }); // how does this work?
+const csrfProtection = csrf({ cookie: true });
+const crypto = require('crypto')
+const session = require('express-session')
 
 // create express app
 const app = express();
@@ -26,19 +23,19 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log("**DB CONNECTED**"))
-  .catch((err) => console.log("DB CONNECTION ERR => ", err)); // to check
+  .catch((err) => console.log("DB CONNECTION ERR => ", err));
+
 
 // apply middlewares
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:3000',
-}));
+app.use(cors({credentials: true, origin: 'https://trial-nine-indol.vercel.app'}));
 app.use(express.json({limit: '5mb'}));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+
+
 // route
-readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`))); // this set the routes
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 // csrf
 app.use(csrfProtection);
 
@@ -46,6 +43,16 @@ app.get("/api/csrf-token", (req, res) => {
   res.json({ csrfToken: req.csrfToken() });
 });
 
+// error handler
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: 'Unauthorized' });
+  } else if (err.name === 'ValidationError') {
+    res.status(422).json({ error: err.message });
+  } else {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 // port
 const port = process.env.PORT || 8000;
